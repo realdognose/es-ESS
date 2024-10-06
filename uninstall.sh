3fbca45049f08d84939b66286f668e42b7f836b7
Daniel Manner <daniel.manner@equinox-solutions.de>
2024-10-06 15:32:53 +0200

    no message

--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,7 @@
 chmod a-x /data/es-ESS/service/run
 kill -s 9 $(pgrep -f 'python /data/es-ESS/es-ESS.py')
 rm -r /data/es-ESS
+
+grep -v "/data/es-ESS/install.sh" /data/rc.local >> /data/temp.local
+mv /data/temp.local /data/rc.local
+chmod 755 /data/rc.local
